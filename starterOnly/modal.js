b6cc423521af84f3ceae716abc298e93a7e14d58
gndz07 <ginadzani@gmail.com>
2020-11-01 21:32:25 +0100

    Adding form verification for name, email, quantity

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,10 +20,65 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+//DOM element for (X) button
+const closeBtn = document.querySelectorAll(".close");
+
+//Close modal function
+function closeModal() {
+	modalbg.style.display = "none";
+}
+
 // close modal form on clicking on close (X) sign
-const closeBtn = document.getElementsByClassName("close");
- closeBtn[0].addEventListener("click", function () {
- 	modalbg.style.display = "none";
- });
+ closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 
+
+ //form verification
+
+ function validate () {
+ 	var firstName = document.getElementById("first").value;
+ 	var lastName = document.getElementById("last").value;
+ 	var email = document.getElementById("email").value;
+ 	var quantity = document.getElementById("quantity").value;
+
+ 	// validation status
+
+ 	var valid = true;
+ 	var message = "";
+
+ 	if (firstName.length <= 2) {
+ 		valid = false;
+ 		message = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+
+ 		document.getElementById("firstResult").textContent = message;
+ 	};
+
+ 	if (lastName.length <= 2) {
+ 		valid = false;
+ 		message = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+
+ 		document.getElementById("lastResult").textContent = message;
+ 	};
+
+ 	var regexEmail = /.+@.+\..+/;
+ 	if (!regexEmail.test(email)) {
+ 		valid = false;
+ 		message = "Veuillez entrer une adresse email valide.";
+
+ 		document.getElementById("emailResult").textContent = message;
+ 	};
+
+ 	var regexQuantity = /\d/;
+ 	if (!regexQuantity.test(quantity)) {
+ 		valid = false;
+ 		message = "Veuillez entrer des chiffres pour nous informer votre précédente(s) participation(s).";
+ 	
+ 		document.getElementById("quantityResult").textContent = message;
+ 	};
+
+
+ 	return valid;
+
+
+ }
